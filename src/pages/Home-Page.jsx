2aa3e75bc88b79ac45aaed8557b57a866f150e37
Arela-Cristina <instagrandine@gmail.com>
2025-03-20 14:56:04 +0100

    millestone 3

--- src/pages/Home-Page.jsx
+++ src/pages/Home-Page.jsx
@@ -1,18 +1,53 @@
-import { useState, useEffect } from "react"
+import React, { useState, useEffect, useMemo } from "react"
 import style from './Home-Page.module.css'
+
+
 
 export default function HomePage() {
 
-    const [politician, setPolitician] = useState([])
-    console.log('risultato fetch', politician)
+    const Politician = React.memo(({ p }) => {
+        console.log("Renderizando Politician:", p.name)
+        return (
+            <figure>
+                <img src={p.image} alt={p.name} />
+                <div className={style.caption}>
+                    <div>Nome: {p.name}</div>
+                    <div>Specie: {p.species}</div>
+                    <div>Biography: {p.gender}</div>
+                </div>
+            </figure>
+        );
+    });
 
+
+    // stato della query input  di ricerca
+    const [query, setQuery] = useState('')
+
+    // stato di partenza per i mostri
+    const [politician, setPolitician] = useState([])
     function fetchPoliticians() {
 
         fetch(`https://rickandmortyapi.com/api/character`)
             .then(response => response.json())
-            .then(data => setPolitician(data.results))
+            .then(data => {
+                setPolitician(data.results)
+            })
+
             .catch(err => console.error(err))
     }
+
+    function handleChangeQuery(e) {
+        setQuery(e.target.value)
+    }
+
+    const filtered = useMemo(() => {
+        return politician.filter(p => {
+            return p.name.toLowerCase().includes(query.toLowerCase()) ||
+                p.species.toLowerCase().includes(query.toLowerCase());
+        });
+    }, [query, politician])
+
+    console.log(filtered)
 
     useEffect(() => {
         fetchPoliticians()
@@ -21,22 +56,20 @@ export default function HomePage() {
 
     return (
         <>
-            <h1>Mostri</h1>
+            <h1>Politicians versione Mostri perche il API di politician era in error 500</h1>
+            <div>
+                <div>Cerca il tuo mostro</div>
+                <input
+                    type="text"
+                    value={query}
+                    onChange={handleChangeQuery} />
+            </div>
+
             <section>
-                {politician.map((p, i) => (
-                    <figure key={i}>
-                        <img src={p.image} alt={p.name} />
-                        <div className={style.caption}>
-                            <div>Nome: {p.name} </div>
-                            <div>Posizione: {p.species}</div>
-                            <div>Biography: {p.gender}</div>
-                        </div>
-
-                    </figure>
+                {filtered.map((p, i) => (
+                    <Politician key={i} p={p} />
                 ))}
-            </section></>
-
-
-
+            </section>
+        </>
     )
 }
